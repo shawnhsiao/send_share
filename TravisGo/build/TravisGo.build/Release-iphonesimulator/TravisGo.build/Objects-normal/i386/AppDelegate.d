0ad58cbb835ff220bffc9b84d0fc3afd92509f9e
shawn.hsiao <shawn.hsiao@aktsk.com>
2016-07-20 11:22:32 +0800

    update

---
+++ TravisGo/build/TravisGo.build/Release-iphonesimulator/TravisGo.build/Objects-normal/i386/AppDelegate.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/shawn/Documents/Xcode\ Project/send_share/TravisGo/TravisGo/AppDelegate.m \
+  /Users/shawn/Documents/Xcode\ Project/send_share/TravisGo/TravisGo/AppDelegate.h
---
+++ TravisGo/build/TravisGo.build/Release-iphonesimulator/TravisGo.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/shawn/Documents/Xcode\ Project/send_share/TravisGo/TravisGo/main.m \
+  /Users/shawn/Documents/Xcode\ Project/send_share/TravisGo/TravisGo/AppDelegate.h
