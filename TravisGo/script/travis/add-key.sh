bbff3e40c043424d19a0bc1bc46f1ff79837cbb7
shawn.hsiao <shawn.hsiao@aktsk.com>
2016-07-20 15:08:35 +0800

    update path

--- TravisGo/script/travis/add-key.sh
+++ TravisGo/script/travis/add-key.sh
@@ -5,12 +5,12 @@ DISTRIBUTION_CERT_NAME="destribution_certificate"
 DISTRIBUTION_KEY_PASSWORD="aktsk"
 
 security create-keychain -p travis $KEYCHAIN_NAME
-security import ./scripts/travis/apple.cer -k ~/Library/Keychains/$KEYCHAIN_NAME -T /usr/bin/codesign
-security import ./scripts/travis/$CERT_NAME.cer -k ~/Library/Keychains/$KEYCHAIN_NAME -T /usr/bin/codesign
-security import ./scripts/travis/$CERT_NAME.p12 -k ~/Library/Keychains/$KEYCHAIN_NAME -P "$KEY_PASSWORD" -T /usr/bin/codesign
+security import TravisGo/script/travis/apple.cer -k ~/Library/Keychains/$KEYCHAIN_NAME -T /usr/bin/codesign
+security import TravisGo/script/travis/$CERT_NAME.cer -k ~/Library/Keychains/$KEYCHAIN_NAME -T /usr/bin/codesign
+security import TravisGo/script/travis/$CERT_NAME.p12 -k ~/Library/Keychains/$KEYCHAIN_NAME -P "$KEY_PASSWORD" -T /usr/bin/codesign
 # used only in ios_aktsk_release
-security import ./scripts/travis/$DISTRIBUTION_CERT_NAME.cer -k ~/Library/Keychains/$KEYCHAIN_NAME -T /usr/bin/codesign
-security import ./scripts/travis/$DISTRIBUTION_CERT_NAME.p12 -k ~/Library/Keychains/$KEYCHAIN_NAME -P "$DISTRIBUTION_KEY_PASSWORD" -T /usr/bin/codesign
+security import TravisGo/script/travis/$DISTRIBUTION_CERT_NAME.cer -k ~/Library/Keychains/$KEYCHAIN_NAME -T /usr/bin/codesign
+security import TravisGo/script/travis/$DISTRIBUTION_CERT_NAME.p12 -k ~/Library/Keychains/$KEYCHAIN_NAME -P "$DISTRIBUTION_KEY_PASSWORD" -T /usr/bin/codesign
 security default-keychain -s $KEYCHAIN_NAME
 # specifies no timeout
 security set-keychain-settings $KEYCHAIN_NAME
