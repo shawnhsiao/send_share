a7441235bc4cf6d3bbbec734cb8f1cd1a6d09bb0
shawn.hsiao <shawn.hsiao@aktsk.com>
2016-07-20 16:12:39 +0800

    update

--- TravisGo/script/travis/remove_key.sh
+++ TravisGo/script/travis/remove_key.sh
@@ -1,5 +1,4 @@
 #bin/bash
 KEYCHAIN_NAME="ios-build.keychain"
-PROFILE_NAME=""
 security delete-keychain $KEYCHAIN_NAME
 rm -f ~/Library/MobileDevice/Provisioning\ Profiles/$PROFILE_NAME.mobileprovision
